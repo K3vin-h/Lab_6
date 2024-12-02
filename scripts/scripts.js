aba90d322e7e677a93b08979c634b7ca6b4dfe37
K3vin <yyc.kevinh@gmail.com>
2024-12-02 10:10:45 -0700

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,5 @@
 /****************** YOUR NAME: 
-
+Kevin Huang
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
 You are encouraged to use the provided naming convention for ease of review.
